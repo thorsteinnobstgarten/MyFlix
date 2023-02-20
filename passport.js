4278d57863884d633fac09e4426af3eb3a5860e3
thorsteinnobstgarten <118816318+thorsteinnobstgarten@users.noreply.github.com>
2023-02-20 15:38:33 +0100

    Update passport.js

--- passport.js
+++ passport.js
@@ -7,38 +7,36 @@ let Users = Models.User,
   JWTStrategy = passportJWT.Strategy,
   ExtractJWT = passportJWT.ExtractJwt;
 
-  passport.use(new LocalStrategy({
-    usernameField: 'Username',
-    passwordfield: 'Password'
-  }, (username, password, callback) => {
-    console.log(username + ' ' + password);
-    Users.findOne({ Username: username }, (error, user) => {
-        if (error) {
-            console.log(error);
-            return callback(error);
-        }
+passport.use(new LocalStrategy({
+  usernameField: 'Username',
+  passwordField: 'Password'
+}, (username, password, callback) => {
+  console.log(username + '  ' + password);
+  Users.findOne({ Username: username }, (error, user) => {
+    if (error) {
+      console.log(error);
+      return callback(error);
+    }
 
-        if (!user) {
-          console.log('incorrect username');
-          return callback(null, false, {message: 'Incorrect username.'
-        });
-        }
+    if (!user) {
+      console.log('incorrect username');
+      return callback(null, false, {message: 'Incorrect username or password.'});
+    }
 
-        console.log('finished');
-        return callback(null, user);
+    console.log('finished');
+    return callback(null, user);
+  });
+}));
+
+passport.use(new JWTStrategy({
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: 'your_jwt_secret'
+}, (jwtPayload, callback) => {
+  return Users.findById(jwtPayload._id)
+    .then((user) => {
+      return callback(null, user);
+    })
+    .catch((error) => {
+      return callback(error)
     });
-  }));
-
-  passport.use(new JWTStrategy({
-    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-    secretOrKey: 'your_jwt_secret'
-  }, (jwtPayload, callback) => {
-    return Users.findById(jwtPayload._id)
-      .then((user) => {
-        return callback(null, user);
-      })
-      .catch((error) => {
-        return callback(error)
-      });
-  }));
-
+}));
