361edcf2310fcd7ef162acf657ce148575c90be3
thorsteinnobstgarten <118816318+thorsteinnobstgarten@users.noreply.github.com>
2023-03-19 16:15:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -191,7 +191,7 @@ app.delete('/users/:Username', passport.authenticate('jwt', { session: false}), 
 
 
 
-app.get('/movies',
+app.get('/movies', passport.authenticate('jwt', { session: false}),
  (req, res) => {
   Movies.find()
     .then((movies) => {
